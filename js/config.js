6e8922e3b88e22caf8d42a6b4e5579ddef711a90
ymahmou <38294241+ymahmou@users.noreply.github.com>
2018-04-18 15:01:55 -0400

    Add files via upload
    updated config file, still needs api

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_lhOTNBd1g',         userPoolClientId: '3s4rr5dlmgrn76o8fukcec48p8',
+        region: 'us-east-2' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
