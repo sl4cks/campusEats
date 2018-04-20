166fb756fc6df93228a6a85a9c30fe627d4e25d3
slacks <sl4cks@users.noreply.github.com>
2018-04-20 14:45:36 -0400

    fixed formatting
    fixed formatting in config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lhOTNBd1g',         userPoolClientId: '3s4rr5dlmgrn76o8fukcec48p8',
-        region: 'us-east-2' 
+        userPoolId: 'us-east-1_lhOTNBd1g',
+        userPoolClientId: '3s4rr5dlmgrn76o8fukcec48p8',
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
