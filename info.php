8767e396cb7b42c934dd1f1bb751b3a001f8dfc5
ymahmou <38294241+ymahmou@users.noreply.github.com>
2018-04-20 17:43:31 -0400

    added info.php file

---
+++ info.php
@@ -0,0 +1,1 @@
+$user_info = “INSERT INTO table_name (username, email) VALUES ('$_POST[username]', '$_POST[email]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); } echo “Your information was added to the database.”; mysql_close($connect); ?> 
