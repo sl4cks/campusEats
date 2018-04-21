ea989e84c5c25a8ffc0df9d1d731994eecde23ad
HarmitSingh001 <35312492+HarmitSingh001@users.noreply.github.com>
2018-04-21 12:52:29 -0400

    Script order form
    for handling order form to database functionality

--- info.php
+++ info.php
@@ -1,1 +1,29 @@
-$user_info = “INSERT INTO table_name (username, email) VALUES ('$_POST[username]', '$_POST[email]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); } echo “Your information was added to the database.”; mysql_close($connect); ?> 
+<?php
+	
+	$formID = rand();
+	$orderName = $_POST['order'];
+	$orderLocation = $_POST['location'];
+	$delivLocation = $_POST['delivery'];
+	$comments = $_POST['comments'];
+	
+	$db_host = '';
+	$db_username = 'campuseats';
+	$db_password = 'campuseats1';
+	$db_name = 'campuseats';
+	
+	mysql_connect($db_host, $db_username, $db_password);
+	mysql_select_db($db_name);
+	
+	$query = "SELECT * FORM 'campuseats'.'FormsDatabase' WHERE 'formID' = '$SformID'";
+	$sqlsearch = mysql_query($query);
+	$resultcount = mysql_numrows($sqlsearch);
+
+	if ($resultcount > 0) {
+		$formID = rand();
+	}
+	
+	mysql_query("INSERT INTO 'campuseats'.'FormsDatabase' ('formID', 'orderName', 'orderLocation', 'delivLocation', 'comments') 
+                               VALUES ('$formID', '$orderName', '$orderLocation', '$delivLocation', '$comments')") 
+    or die(mysql_error());  
+		
+?>
