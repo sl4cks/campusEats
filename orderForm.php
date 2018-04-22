54fea6a75112b408465358aeb2b7361d783073f2
alb <alibroyles@gmail.com>
2018-04-22 14:47:25 -0400

    moved php

--- orderForm.php
+++ orderForm.php
@@ -1,4 +1,39 @@
 <!DOCTYPE html>
+<?php
+	$formID = rand();
+	$orderName = $_POST['order'];
+	$orderLocation = $_POST['location'];
+	$delivLocation = $_POST['delivery'];
+	$comments = $_POST['comments'];
+
+	$db_host = 'campuseatsdb.cbbdidcdihbq.us-east-1.rds.amazonaws.com'; //server name
+	$db_username = 'campuseats';
+	$db_password = 'campuseats1';
+	$db_name = 'campuseats';
+
+	$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+	if (!$conn) {
+		die ('Failed to connect to MySQL: ' . mysqli_connect_error());
+	}
+
+	$sql = 'SELECT * FROM FormsDatabase';
+	$query = mysqli_query($conn, $sql);
+
+	if (!$query) {
+		die ('SQL Error: ' . mysqli_error($conn));
+	}
+
+	$resultcount = mysql_numrows($sqlsearch);
+
+	if ($resultcount > 0) {
+		$formID = rand();
+	}
+
+	mysql_query("INSERT INTO 'campuseats'.'FormsDatabase' ('formID', 'orderName', 'orderLocation', 'delivLocation', 'comments')
+															 VALUES ('$formID', '$orderName', '$orderLocation', '$delivLocation', '$comments')")
+		or die(mysql_error());
+
+?>
 <html>
 <head>
 	<title>Campus Eats - Order</title>
@@ -77,42 +112,8 @@
 				</div>
 		</form>
     </div>
-		<?php
 
-			$formID = rand();
-			$orderName = $_POST['order'];
-			$orderLocation = $_POST['location'];
-			$delivLocation = $_POST['delivery'];
-			$comments = $_POST['comments'];
 
-			$db_host = 'campuseatsdb.cbbdidcdihbq.us-east-1.rds.amazonaws.com'; //server name
-			$db_username = 'campuseats';
-			$db_password = 'campuseats1';
-			$db_name = 'campuseats';
-
-			$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-			if (!$conn) {
-				die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-			}
-
-			$sql = 'SELECT * FROM FormsDatabase';
-			$query = mysqli_query($conn, $sql);
-
-			if (!$query) {
-				die ('SQL Error: ' . mysqli_error($conn));
-			}
-
-			$resultcount = mysql_numrows($sqlsearch);
-
-			if ($resultcount > 0) {
-				$formID = rand();
-			}
-
-			mysql_query("INSERT INTO 'campuseats'.'FormsDatabase' ('formID', 'orderName', 'orderLocation', 'delivLocation', 'comments')
-		                               VALUES ('$formID', '$orderName', '$orderLocation', '$delivLocation', '$comments')")
-		    or die(mysql_error());
-
-		?>
   </div>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
