f8aa81a518cfb60782866a0f29f6e220c6755966
alb <alibroyles@gmail.com>
2018-04-22 16:18:06 -0400

    updated to .php links
    updated links to reflect change in pages from .html to .php

--- problemReporting.php
+++ problemReporting.php
@@ -58,14 +58,14 @@
 			        <span class="icon-bar"></span>
 			    	<span class="icon-bar"></span>
 			    </button>
-				<a class="navbar-brand" href="index.html"></span>  Campus Eats</a>
+				<a class="navbar-brand" href="index.php"></span>  Campus Eats</a>
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar">
-					<li><a href="about.html">About</a></li>
+					<li><a href="about.php">About</a></li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="index.html">Log Out</a></li>
+					<li><a href="index.php">Log Out</a></li>
 				</ul>
 			</div>
 		</div>
