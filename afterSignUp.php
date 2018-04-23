cbd16aa3dcbdddfbe3f6f8f09a6a1797f650133a
ymahmou <38294241+ymahmou@users.noreply.github.com>
2018-04-22 20:35:07 -0400

    update email

--- afterSignUp.php
+++ afterSignUp.php
@@ -20,7 +20,7 @@
 																	VALUES ('$uID', '$user', '$pass', 0, 5)")
 			or die(mysqli_error($conn));
 
-		$to = $user."@gmu.edu"; // this is your Email address
+		$to = $_POST['netID']."@gmu.edu"; // this is your Email address
 		$from = "ymahmou@masonlive.gmu.edu"; // this is the sender's Email address
 		$subject = "Please Confirm you CampusEats Account";
 		$message = "Thanks for signing up for CampusEats, Here's your verification code: ".$uID.
