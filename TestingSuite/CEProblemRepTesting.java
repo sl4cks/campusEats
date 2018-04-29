810e81c78ead0a36d304d29d3e45d94a0e6c10d4
HarmitSingh001 <35312492+HarmitSingh001@users.noreply.github.com>
2018-04-29 17:03:07 -0400

    file fixes

--- TestingSuite/CEProblemRepTesting.java
+++ TestingSuite/CEProblemRepTesting.java
@@ -15,13 +15,53 @@ class CEProblemRepTesting {
 		File file = new File("/Users/Harry/Desktop/chromedriver");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 		driver = new ChromeDriver();
-		driver.get("http://ec2-54-210-100-190.compute-1.amazonaws.com/deliveryForm.php");
+		driver.get("http://ec2-54-210-100-190.compute-1.amazonaws.com");
 	}
 
-
+	// sample report test
 	@Test
-	void problemRepTesting1() {
-
+	void problemRepTesting1() throws InterruptedException {
+		WebElement reporter = driver.findElement(By.xpath("//*[@id=\"orderNumber\"]"));
+		reporter.click();
+		Thread.sleep(1000);
+		reporter.sendKeys("This is my order number");
+		Thread.sleep(1000);
+		reporter = driver.findElement(By.xpath("//*[@id=\"problemdescription\"]"));
+		reporter.click();
+		Thread.sleep(1000);
+		reporter.sendKeys("This is my report for this guy ouch");
+		reporter = driver.findElement(By.xpath("//*[@id=\"submitform\"]"));
+		Thread.sleep(1000);
+		reporter.click();
+		driver.close();
 	}
-
+	
+	// go home test
+	@Test
+	void problemRepTesting2() throws InterruptedException{
+		WebElement reporter = driver.findElement(By.xpath("/html/body/nav/div/div[1]/a"));
+		Thread.sleep(1000);
+		reporter.click();
+		driver.close();
+	}
+	
+	// about test
+	@Test
+	void problemRepTesting3() throws InterruptedException {
+		WebElement reporter = driver.findElement(By.xpath("//*[@id=\"bs-example-navbar-collapse-1\"]/ul[1]/li/a"));
+		Thread.sleep(1000);
+		reporter.click();
+		driver.close();
+	}
+	
+	
+	// log out test
+	@Test
+	void problemRepTesting4() throws InterruptedException {
+		WebElement reporter = driver.findElement(By.xpath("///*[@id=\"bs-example-navbar-collapse-1\"]/ul[2]/li/a"));
+		Thread.sleep(1000);
+		reporter.click();
+		driver.close();
+	}
+	
 }
